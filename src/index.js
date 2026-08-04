83dd49378026228b45c253c75c0f85440df80606
missyouling <missyouling@users.noreply.github.com>
2026-08-04 09:30:46 +0000

    chore: 前端产物同步（收入拆三列 + 各表合计行）

--- src/index.js
+++ src/index.js
@@ -414,7 +414,7 @@ const SPA_HTML = `<!doctype html>
   <head><meta charset="UTF-8" /><link rel="icon" type="image/svg+xml" href="/favicon.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>综合管理平台</title>
-    <script type="module" crossorigin src="/assets/index-Dtwgexud.js"></script>
+    <script type="module" crossorigin src="/assets/index-BhBBrAS_.js"></script>
     <link rel="stylesheet" crossorigin href="/assets/index-Dn46icR7.css">
   </head>
   <body><div id="root"></div></body>
